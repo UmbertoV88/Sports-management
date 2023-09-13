b37b0081d277e59eaa3519477cf13fdab810dedf
UmbertoV88 <umbertomaria.vellonio@gmail.com>
2023-09-13 16:02:46 +0200

    Create the various classes and define the entire applicatios

--- app.js
+++ app.js
@@ -5,3 +5,34 @@ giocatore3 = new Giocatore("Roberto", "Neri");
 allenatore = new Allenatore("Roberto", "Mancini", 2008);
 
 squadra = new Squadra("Le Acquile",allenatore);
+
+giocatore1.nuoviPunti(3);
+giocatore1.nuoviPunti(2);
+
+giocatore2.nuoviPunti(7);
+
+giocatore3.nuoviPunti(1);
+giocatore3.nuoviPunti(1);
+
+squadra.nuovoGiocatore(giocatore1);
+squadra.nuovoGiocatore(giocatore2);
+squadra.nuovoGiocatore(giocatore3);
+
+squadra.stampaSquadra();
+
+document.write("<br><br>");
+document.write("Miglior cannoniere: " + squadra.capocannoniere.descrizione + "<br><br>");
+document.write("<br><br>");
+
+if(squadra.rimuoviGiocatore(giocatore2)){
+    document.write("<br> Giocatore rimosso <br>");
+} else {
+    document.write("<br> Giocatore non presente <br>");
+}
+document.write("<br><br>");
+
+squadra.stampaSquadra();
+
+document.write("<br><br>");
+
+document.write("Miglior cannoniere: " + squadra.capocannoniere.descrizione + "<br><br>");
--- classes.js
+++ classes.js
@@ -3,12 +3,28 @@ class Persona {
         this.nome = nome;
         this.cognome = cognome;
     }
+
+    get nomeCompleto(){
+        return `${this.nome} ${this.cognome}`;
+    }
 };
 
 class Giocatore extends Persona {
     constructor(nome, cognome) {
         super(nome,cognome);
         this.punteggi = 0;
+    }
+
+    nuoviPunti(punti) {
+        this.punteggi += punti;
+    }
+
+    get punteggio() {
+        return this.punteggi;
+    }
+
+    get descrizione() {
+        return super.nomeCompleto + "-" + this.punteggi;
     }
 };
 
@@ -17,12 +33,45 @@ class Allenatore extends Persona {
         super(nome,cognome);
         this.anno_assunzione = anno;
     }
+
+    get descrizione() {
+        return `${this.nomeCompleto} (dal ${this.anno_assunzione})`;
+    }
 };
 
 class Squadra {
     constructor(nome, allenatore){
         this.nome = nome;
         this.allenatore = allenatore;
-        this.giocari = new Set();
+        this.giocatori = new Set();
     }
+
+    nuovoGiocatore(giocatore) {
+        this.giocatori.add(giocatore);
+    }
+
+    rimuoviGiocatore(giocatore){
+        return this.giocatori.delete(giocatore);
+    }
+
+    stampaSquadra() {
+        document.write(`Allena: ${allenatore.descrizione} <br>`)
+        document.write(`<br>Formazione: (${this.giocatori.size} giocatori) <br>`);
+
+        for(let g of this.giocatori){
+            document.write(g.descrizione);
+            document.write("<br>");
+        }       
+    }
+
+    get capocannoniere() {
+        let capocannoniere;
+        for (let g of this.giocatori) {
+            if(capocannoniere == null || capocannoniere.punteggio < g.punteggio) {
+                capocannoniere = g;
+            }
+        }
+        return capocannoniere;
+    }
+
 }
