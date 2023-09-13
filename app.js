b8395c9782c733d5b193d972a0b7a2ae0ad12927
UmbertoV88 <umbertomaria.vellonio@gmail.com>
2023-09-13 14:32:14 +0200

    testing

--- app.js
+++ app.js
@@ -4,4 +4,4 @@ giocatore3 = new Giocatore("Roberto", "Neri");
 
 allenatore = new Allenatore("Roberto", "Mancini", 2008);
 
-squadra = new Squadra("Le Acquile",allenatore)
+squadra = new Squadra("Le Acquile",allenatore);
